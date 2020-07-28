95a368e8c8cb87e27a139d0569aaf786aa389951
shurawi <denis.b.toropov@gmail.com>
2020-07-28 22:29:23 +0700

    prepares the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,41 @@
 // Include gulp
-var gulp = require('gulp'),
+var gulp = require('gulp');
   // Include Our Plugins
-  less = require('gulp-less'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+var less = require('gulp-less');
+var postcss = require('gulp-postcss');
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var autoprefixer = require('autoprefixer');
+var csso = require("gulp-csso");
+var server = require("browser-sync").create();
 
-gulp.task('css', function () {
-  gulp
-    .src('./less/main.less')
-    .pipe(less({ strictMath: true }))
+gulp.task("csssource", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
     .pipe(postcss([
       autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
     ]))
-    .pipe(gulp.dest('./css'));
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
+gulp.task("serversource", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("csssource"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task('default', ['css', 'watch']);
+gulp.task("startsource", gulp.series("csssource", "serversource"));
